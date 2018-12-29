536b6a021f3e323bfba0f08f76a76cb07c3cfb29
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2018-12-29 18:27:43 +0530

    gwcko4

--- src/test/java/com/sample/Basics/Invoke.java
+++ src/test/java/com/sample/Basics/Invoke.java
@@ -1,5 +1,7 @@
 package com.sample.Basics;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,7 +11,7 @@ public class Invoke {
 	
 	@Test
 	public void invokeApp(){
-		System.setProperty("webdriver.gecko.driver", "E:\\Devops27\\Basics\\Drivers\\geckodriver");
+		System.setProperty("webdriver.gecko.driver",String.format(System.getProperty("user.dir")+"%sDrivers%sgeckodriver",File.pathSeparator));
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("marionette", true);
 		WebDriver wbDriver = new FirefoxDriver(capabilities);
