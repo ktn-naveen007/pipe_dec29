1e8071174159bf879dd40e6b6b86ae0eba2f0511
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2018-12-31 21:40:10 +0530

    pack2

--- src/test/java/com/sample/Basics/Invoke.java
+++ src/test/java/com/sample/Basics/Invoke.java
@@ -5,11 +5,13 @@ import java.io.File;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class Invoke {
 	
 	@Test
+	@Ignore
 	public void invokeApp(){
 		System.setProperty("webdriver.gecko.driver",String.format(System.getProperty("user.dir")+"%sDrivers%sgeckodriver",File.separatorChar,File.separatorChar));
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
